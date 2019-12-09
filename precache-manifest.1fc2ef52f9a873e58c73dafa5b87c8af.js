57f12fba5449c824645aa572c9dfd7227f2d048d
DeyviTabora <taborapaz@gmail.com>
2019-12-09 17:11:36 -0600

    Updates

---
+++ precache-manifest.1fc2ef52f9a873e58c73dafa5b87c8af.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1564e74bc9c471045ebc3028dfd6e60a",
+    "url": "/grintsys.website/index.html"
+  },
+  {
+    "revision": "08aad1aac905447edf15",
+    "url": "/grintsys.website/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "aca3af10c27882d9247c",
+    "url": "/grintsys.website/static/css/main.a3f29b1a.chunk.css"
+  },
+  {
+    "revision": "08aad1aac905447edf15",
+    "url": "/grintsys.website/static/js/2.d57a50ad.chunk.js"
+  },
+  {
+    "revision": "aca3af10c27882d9247c",
+    "url": "/grintsys.website/static/js/main.f384c2d7.chunk.js"
+  },
+  {
+    "revision": "3f43d589b4cf6c01676a",
+    "url": "/grintsys.website/static/js/runtime~main.b56810e2.js"
+  },
+  {
+    "revision": "ad95630f90d1d6386bc2c5aec792eca0",
+    "url": "/grintsys.website/static/media/billdollar.ad95630f.png"
+  },
+  {
+    "revision": "ea2db99ee1dfa820f038ca34fd9b3267",
+    "url": "/grintsys.website/static/media/cart.ea2db99e.png"
+  },
+  {
+    "revision": "7839df68ba3433de2ed802907e26dcf1",
+    "url": "/grintsys.website/static/media/cart_summary.7839df68.png"
+  },
+  {
+    "revision": "b50f8e0899bb66aa235186d9128e895d",
+    "url": "/grintsys.website/static/media/clouds.b50f8e08.png"
+  },
+  {
+    "revision": "e3ec4d57158b09fe705320cf2ca7deda",
+    "url": "/grintsys.website/static/media/curly.e3ec4d57.png"
+  },
+  {
+    "revision": "131966a74fc7ffae8af0893dbd8a2a5f",
+    "url": "/grintsys.website/static/media/database.131966a7.png"
+  },
+  {
+    "revision": "096f87cf0af78634e3b93afb90da8adc",
+    "url": "/grintsys.website/static/media/inventory.096f87cf.png"
+  },
+  {
+    "revision": "f9651f282be685c060d18c3d42ed0089",
+    "url": "/grintsys.website/static/media/phone1.f9651f28.png"
+  },
+  {
+    "revision": "122a37b1b736a0ce8b24cd13d58c2619",
+    "url": "/grintsys.website/static/media/startup.122a37b1.png"
+  },
+  {
+    "revision": "878b65643fc3a2305688bc83ea8b48bf",
+    "url": "/grintsys.website/static/media/stats2.878b6564.png"
+  },
+  {
+    "revision": "7cc47bb4bedc5682a91d88ac6ab369ae",
+    "url": "/grintsys.website/static/media/update.7cc47bb4.png"
+  },
+  {
+    "revision": "319003d34440f65ed13235fbd1b129aa",
+    "url": "/grintsys.website/static/media/user.319003d3.png"
+  }
+]);
