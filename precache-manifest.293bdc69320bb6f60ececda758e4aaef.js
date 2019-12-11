43c0f58cbb6aad988167beb060d698d35ffb2bfe
DeyviTabora <taborapaz@gmail.com>
2019-12-11 10:23:05 -0600

    Updates

---
+++ precache-manifest.293bdc69320bb6f60ececda758e4aaef.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d01bed335042bfce850d7029a7aa1f",
+    "url": "/Grintsys/index.html"
+  },
+  {
+    "revision": "ef814f9c4dc481606b8d",
+    "url": "/Grintsys/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "738291ac00e7a2a31433",
+    "url": "/Grintsys/static/css/main.9a38ed14.chunk.css"
+  },
+  {
+    "revision": "ef814f9c4dc481606b8d",
+    "url": "/Grintsys/static/js/2.e91bb374.chunk.js"
+  },
+  {
+    "revision": "c8b7c95e0de77ac22255d5dee7031440",
+    "url": "/Grintsys/static/js/2.e91bb374.chunk.js.LICENSE"
+  },
+  {
+    "revision": "738291ac00e7a2a31433",
+    "url": "/Grintsys/static/js/main.73b7f49c.chunk.js"
+  },
+  {
+    "revision": "476898c97f10d6623902",
+    "url": "/Grintsys/static/js/runtime-main.14a05591.js"
+  },
+  {
+    "revision": "ad95630f90d1d6386bc2c5aec792eca0",
+    "url": "/Grintsys/static/media/billdollar.ad95630f.png"
+  },
+  {
+    "revision": "ea2db99ee1dfa820f038ca34fd9b3267",
+    "url": "/Grintsys/static/media/cart.ea2db99e.png"
+  },
+  {
+    "revision": "7839df68ba3433de2ed802907e26dcf1",
+    "url": "/Grintsys/static/media/cart_summary.7839df68.png"
+  },
+  {
+    "revision": "b50f8e0899bb66aa235186d9128e895d",
+    "url": "/Grintsys/static/media/clouds.b50f8e08.png"
+  },
+  {
+    "revision": "e3ec4d57158b09fe705320cf2ca7deda",
+    "url": "/Grintsys/static/media/curly.e3ec4d57.png"
+  },
+  {
+    "revision": "131966a74fc7ffae8af0893dbd8a2a5f",
+    "url": "/Grintsys/static/media/database.131966a7.png"
+  },
+  {
+    "revision": "096f87cf0af78634e3b93afb90da8adc",
+    "url": "/Grintsys/static/media/inventory.096f87cf.png"
+  },
+  {
+    "revision": "f9651f282be685c060d18c3d42ed0089",
+    "url": "/Grintsys/static/media/phone1.f9651f28.png"
+  },
+  {
+    "revision": "122a37b1b736a0ce8b24cd13d58c2619",
+    "url": "/Grintsys/static/media/startup.122a37b1.png"
+  },
+  {
+    "revision": "878b65643fc3a2305688bc83ea8b48bf",
+    "url": "/Grintsys/static/media/stats2.878b6564.png"
+  },
+  {
+    "revision": "7cc47bb4bedc5682a91d88ac6ab369ae",
+    "url": "/Grintsys/static/media/update.7cc47bb4.png"
+  },
+  {
+    "revision": "319003d34440f65ed13235fbd1b129aa",
+    "url": "/Grintsys/static/media/user.319003d3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74c83337ffec9720cb2babb6c6eef94e.js"
+  "/Grintsys/precache-manifest.293bdc69320bb6f60ececda758e4aaef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Grintsys/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
